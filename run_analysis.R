16156846b3fa9cc3331b0878e942bb39d9c36958
waldmanz <waldmanz@yahoo.com>
2014-05-19 17:14:03 -0400

    Work in progress

---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+
+
+# setwd("~/Dropbox/DataScience/DataCleaning/courseproject/gettingcleaningdata")
+
+### read data with labels for activities and features
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt",
+                       col.names = c("Activity_Number", "Activity_Label"))
+
+features <- read.table("./UCI HAR Dataset/features.txt",
+                              col.names = c("Feature_Number", "Feature_Name"))
+
+### read subject train and test data (as factor) and combine
+
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt",
+                            col.names = "Subject", colClasses="factor")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt",
+                           col.names = "Subject", colClasses="factor")
+subject_combined <- rbind(subject_train, subject_test)
+
+# read y data (activity number) for train and test data and combine
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt",
+                     col.names = "Activity_Number")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt",
+                     col.names = "Activity_Number")
+y_combined <- rbind(y_train, y_test)
+
+
+# read X data for train and test and combine (takes a while)
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+X_combined <- rbind(X_train, X_test)
+
+# use data from features data frame to assign column names to features
+names(X_combined) <- features$Feature_Name
+
+# create index to select mean and std features
+# note \\ used to escape open and closed parenthesis
+index <- grep("mean\\(\\)|std\\(\\)", features$Feature_Name, ignore.case = TRUE)
+# features[index,2]
+
+# subset X data to select columns with mean and std data
+X_combined_mean_std <- X_combined[,index]
+
+# use merge to add activity names (implicitly, by="Activity_Number",
+#   because this is the common column name)
+#   all.x = TRUE gives output of length y_test
+
+y_combined_merged <- merge(y_combined, activities, all.x = TRUE)
+
+# create data frame with subject, activity number, activity label
+tidy_frame1 <- data.frame(subject_combined, y_combined_merged)
+tidy_frame1 <- data.frame(tidy_frame1, X_combined_mean_std)
+
+# create vector of new column names for 2nd tidy data frame
+# Add "Average" to the old column names
+
+newnames <- paste("Average", colnames(tidy_frame1))
+newframetemp <- split(tidy_frame1, f=list(tidy_frame1$Subject,
+                                          tidy_frame1$Activity_Label))
